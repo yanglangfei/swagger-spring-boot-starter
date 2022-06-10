57d4198b62a8c5af947a6ab0a9eaeef8a45d84e6
ylf <legaoyun@gmail.com>
2022-06-10 15:01:31 +0800

    1

--- src/main/java/com/yanglf/config/SwaggerConfig.java
+++ src/main/java/com/yanglf/config/SwaggerConfig.java
@@ -38,7 +38,7 @@ public class SwaggerConfig {
                 // 设置哪些接口暴露给Swagger展示
                 .select()
                 // 扫描所有有注解的api，用这种方式更灵活
-                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                //.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
                 // 扫描指定包中的swagger注解
                 .apis(RequestHandlerSelectors.basePackage(properties.getBasePackage()))
                 .build()
